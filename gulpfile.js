fe55ec9da831e3a67d1acb87996ccf15dfe45bf7
nizo <martin.niznansky@gmail.com>
2019-05-20 14:40:18 +0200

    semantic versioning via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,20 @@
-// `fs` is used instead of require to prevent caching in watch (require caches)
+var gulp = require('gulp');
+var bump = require('gulp-bump');
+var replace = require('gulp-replace');
 var fs = require('fs');
 var semver = require('semver');
 
-var getPackageJson = function () {
-	return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-};
-
-// bump versions on package/bower/manifest
-gulp.task('bump', function () {
-  // reget package
-	var pkg = getPackageJson();
-	// increment version
+gulp.task('default', function(){
+	var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 	var newVer = semver.inc(pkg.version, 'patch');
 
-	// uses gulp-filter
-	var manifestFilter = tasks.filter(['manifest.json']);
-	var regularJsons = tasks.filter(['!manifest.json']);
-
-	return gulp.src(['./bower.json', './package.json', './src/manifest.json'])
-	.pipe(tasks.bump({
-		version: newVer
-	}))
-	.pipe(manifestFilter)
-	.pipe(gulp.dest('./src'))
-	.pipe(manifestFilter.restore())
-	.pipe(regularJsons)
+	gulp.src('./package.json')
+		.pipe(bump({
+			version: newVer
+		}))
 	.pipe(gulp.dest('./'));
-});
 
-// Run the gulp tasks
-gulp.task('default', function(){
-	gulp.run('bump');
+	return gulp.src(['style.css'])
+		.pipe(replace(/Version: .*/, 'Version: ' + newVer))
+		.pipe(gulp.dest('./'));
 });
