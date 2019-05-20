28cfa61f76d766f4a98ec61a25a4a7ff848bd5fc
nizo <martin.niznansky@gmail.com>
2019-05-20 15:46:37 +0200

    automatic versioning - test 01

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var bump = require('gulp-bump');
 var replace = require('gulp-replace');
 var fs = require('fs');
 var semver = require('semver');
+var git = require('gulp-git');
 
-gulp.task('default', function(){
+
+gulp.task('bump-version', function(){
 	var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 	var newVer = semver.inc(pkg.version, 'patch');
 
@@ -14,7 +16,12 @@ gulp.task('default', function(){
 		}))
 	.pipe(gulp.dest('./'));
 
-	return gulp.src(['style.css'])
+	gulp.src('./style.css')
 		.pipe(replace(/Version: .*/, 'Version: ' + newVer))
 		.pipe(gulp.dest('./'));
+
+	return gulp.src(['./package.json', './style.css'])
+		.pipe(git.add());
 });
+
+gulp.task('pre-commit',  gulp.series('bump-version'));
