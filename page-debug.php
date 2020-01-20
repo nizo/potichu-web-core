3bc08c1ae996d96b26f2df9991ab4133a429bee8
nizo <martin.niznansky@gmail.com>
2020-01-20 22:29:06 +0100

    breadcrumbs

---
+++ page-debug.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	$pages = get_pages();
+	foreach ($pages as $page) {
+		update_post_meta($page->ID, 'header', 'yes');
+		update_post_meta($page->ID, 'header_title_bar', 'breadcrumbs_only');
+		//echo get_post_meta($page->ID, 'header_title_bar', true) . ' ----';
+	}
+	*/
+?>
